833c938b9a705049ac6fe7558cd39b644feeca35
Mike Pearce <mike@mikepearce.net>
2012-02-03 12:55:08 +0000

    Removed the PHP and added the getclicky code

--- theme/r15n/index.php
+++ theme/r15n/index.php
@@ -70,7 +70,9 @@ const OVERLAY_BOTTOM_HTML = <<<HTML
 HTML;
 
 const END_HTML = <<<HTML
-<?php wp_footer(); ?>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(66527447); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/66527447ns.gif" /></p></noscript>
 </body>
 </html>
 HTML;
